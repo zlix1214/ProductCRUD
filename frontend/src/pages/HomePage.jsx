5d976cdfa9a56b49a4136ccf0ccb7dd5e1e60d51
rraefelix@gmail.com <rraefelix@gmail.com>
2025-02-04 21:19:42 +0800

    change the title

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,52 +5,62 @@ import { useProductStore } from "../store/product";
 import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
-	const { fetchProducts, products } = useProductStore();
+  const { fetchProducts, products } = useProductStore();
 
-	useEffect(() => {
-		fetchProducts();
-	}, [fetchProducts]);
-	console.log("products", products);
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
 
-	return (
-		<Container maxW='container.xl' py={12}>
-			<VStack spacing={8}>
-				<Text
-					fontSize={"30"}
-					fontWeight={"bold"}
-					bgGradient={"linear(to-r, cyan.400, blue.500)"}
-					bgClip={"text"}
-					textAlign={"center"}
-				>
-					Current Products
-				</Text>
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Welcome to add, edit, or delete your own products, but
+          please avoid adding inappropriate items. Thank you XD
+        </Text>
 
-				<SimpleGrid
-					columns={{
-						base: 1,
-						md: 2,
-						lg: 3,
-					}}
-					spacing={10}
-					w={"full"}
-				>
-					{products.map((product) => (
-						<ProductCard key={product._id} product={product} />
-					))}
-				</SimpleGrid>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
 
-				{products.length === 0 && (
-					<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
-						No products found 😢{" "}
-						<Link to={"/create"}>
-							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
-								Create a product
-							</Text>
-						</Link>
-					</Text>
-				)}
-			</VStack>
-		</Container>
-	);
+        {products.length === 0 && (
+          <Text
+            fontSize="xl"
+            textAlign={"center"}
+            fontWeight="bold"
+            color="gray.500"
+          >
+            No products found 😢{" "}
+            <Link to={"/create"}>
+              <Text
+                as="span"
+                color="blue.500"
+                _hover={{ textDecoration: "underline" }}
+              >
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+    </Container>
+  );
 };
 export default HomePage;
